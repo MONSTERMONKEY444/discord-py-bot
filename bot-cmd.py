56a1b35149f1958449bb65b4a26de2e91ef4216d
JORDANHUNT28828 <122761864+MONSTERMONKEY444@users.noreply.github.com>
2024-07-13 21:11:07 -0700

    Update bot-cmd.py

--- bot-cmd.py
+++ bot-cmd.py
@@ -1,10 +1,8 @@
 import discord
 import asyncio
 import pyfiglet
-from termcolor import colored
-from colorama import init
+from pystyle import Center, Colors, Colorate
 
-init()
 
 # Function to read bot tokens from a file
 def read_bot_tokens(filename):
@@ -14,15 +12,14 @@ def read_bot_tokens(filename):
 
 # BOT_TOKENS set to bot tokens in 'bot-tokens.txt'
 BOT_TOKENS = read_bot_tokens('bot-tokens.txt')
+print(f'Loaded {len(BOT_TOKENS)} Bot Tokens!')
 
 class Client(discord.Client):
-    # Funny Constructor
     def __init__(self, *args, shared_params, counters, **kwargs):
         super().__init__(*args, **kwargs)
         self.shared_params = shared_params
         self.counters = counters
     
-    # Runs when Bots are present
     async def on_ready(self):
         print(f'BOT LOGGED IN AND LOADED AS: {self.user}')
         choice = self.shared_params['choice']
@@ -41,7 +38,6 @@ class Client(discord.Client):
             print("Invalid choice.")
         await self.close()
 
-    # DM Function
     async def send_dm(self):
         try:
             USER_ID = self.shared_params['USER_ID']
@@ -67,7 +63,6 @@ class Client(discord.Client):
         except discord.HTTPException as e:
             print(f'Failed to perform action: {e}')
 
-    # Spam a Singular Channel with Messages, or Messages with an Image
     async def spam_channel_messages(self):
         try:
             CHANNEL_ID = self.shared_params['CHANNEL_ID']
@@ -102,7 +97,6 @@ class Client(discord.Client):
         except discord.HTTPException as e:
             print(f'Failed to perform action: {e}')
 
-     # Creates Text or Voice Channels
     async def create_channel(self):
         try:
             CHANNEL_NAME = self.shared_params['CHANNEL_NAME']
@@ -132,7 +126,6 @@ class Client(discord.Client):
         except discord.HTTPException as e:
             print(f'Failed to perform action: {e}')
 
-    # Deletes all types of Channels (Text & Voice)
     async def delete_all_channels(self):
         try:
             guild = discord.utils.get(self.guilds, id=self.shared_params['guild_id'])
@@ -151,7 +144,6 @@ class Client(discord.Client):
         except discord.HTTPException as e:
             print(f'Failed to perform action: {e}')
 
-    # Sends messages to all channels (1 by 1, can be with Images)
     async def send_message_all_channels(self):
         try:
             guild = discord.utils.get(self.guilds, id=self.shared_params['guild_id'])
@@ -165,7 +157,6 @@ class Client(discord.Client):
             DELAY_TIME = self.shared_params['DELAY_TIME']
             NUM_MESSAGES_PER_CHANNEL = self.shared_params['NUM_MESSAGES_PER_CHANNEL']
 
-            # Nested For Loop for NUM_MESSAGES in each Channel
             for channel in channels:
                 try:
                     for _ in range(NUM_MESSAGES_PER_CHANNEL):
@@ -188,68 +179,103 @@ class Client(discord.Client):
             print(f'Cannot perform action due to lack of permissions!')
         except discord.HTTPException as e:
             print(f'Failed to perform action: {e}')
+            
 
-# Main
+async def check_guild_exists(guild_id, bot_token):
+        intents = discord.Intents.default()
+        client = discord.Client(intents=intents)
+        try:
+            await client.login(bot_token)
+            guild = await client.fetch_guild(guild_id)
+            await client.close()
+            return guild is not None
+        except discord.NotFound:
+            await client.close()
+            return False
+        except Exception as e:
+            await client.close()
+            print(f"Error: {e}")
+            return False
+
 async def main():
-    guild_id = int(input("Enter Discord Server ID: "))
-    shared_params = {'guild_id': guild_id}
-    counters = {"messages": 0, "channels": 0, "channels_deleted": 0}
+        valid_guild = False
+        guild_id = None
+        while not valid_guild:
+            try:
+                guild_id = int(input("Enter Discord Server ID: "))
+                for token in BOT_TOKENS:
+                    valid_guild = await check_guild_exists(guild_id, token)
+                    if valid_guild:
+                        break   
+                if not valid_guild:
+                    print("Invalid server ID. Please try again.")
+            except ValueError:
+                print("Invalid input. Please enter a valid server ID.")
+        shared_params = {'guild_id': guild_id}
+        counters = {"messages": 0, "channels": 0, "channels_deleted": 0}
 
-    while True:
-        # ASCII Art
-        text1 = "Sierra Is A"
-        figlet_text1 = pyfiglet.figlet_format(text1, font="standard")
-        colored_text1 = colored(figlet_text1, color="yellow", on_color="on_red", attrs=['bold'])
-        print(f"{colored_text1}")
+        while True:
+            text1 = "SIERRA IS A"
+            figlet_text1 = pyfiglet.figlet_format(text1, font="5lineoblique")
+            gradient_text1 = Colorate.Color(Colors.blue, figlet_text1, True)
+            centered_text1 = Center.XCenter(gradient_text1)
+            print(centered_text1)
 
-        text2 = "TERRORIST"
-        figlet_text2 = pyfiglet.figlet_format(text2, font="letters")
-        colored_text2 = colored(figlet_text2, color="yellow", on_color="on_red", attrs=['bold'])
-        print(f"{colored_text2}")
-        
-        print("1 For Direct Messages")
-        print("2 For Text Channel Messages")
-        print("3 For Create Channels")
-        print("4 For Deleting All Channels")
-        print("5 For All Text Channel Messages")
-        print("Note: More Bot Tokens Multiple the Amount you input!")
-        choice = int(input("Choose an action: "))
-        shared_params['choice'] = choice
+            text2 = "TERRORIST"
+            figlet_text2 = pyfiglet.figlet_format(text2, font="standard")
+            gradient_text2 = Colorate.Color(Colors.red, figlet_text2, True)
+            centered_text2 = Center.XCenter(gradient_text2)
+            print(centered_text2)
+            
+            print(Center.XCenter("[0 For Exiting]"))
+            print(Center.XCenter("[1 For Direct Messages]"))
+            print(Center.XCenter("[2 For Text Channel Messages]"))
+            print(Center.XCenter("[3 For Create Channels]"))
+            print(Center.XCenter("[4 For Deleting All Channels]"))
+            print(Center.XCenter("[5 For All Text Channel Messages]"))
+            print("")
+            print("Note: More Bot Tokens Multiply The Amount You Input!")
+            choice = int(input("Choose an action: "))
+            shared_params['choice'] = choice
 
-        if choice == 1:
-            shared_params['USER_ID'] = int(input("Enter the user ID to send messages to: "))
-            shared_params['MESSAGE'] = input("Enter the message you want to send: ")
-            shared_params['NUM_MESSAGES'] = int(input("Number of messages: "))
-            shared_params['PHOTO_URL'] = input("Enter an Image Address to send (leave empty if no photo): ")
-            shared_params['DELAY_TIME'] = float(input("Enter the delay between messages in seconds: "))
-        elif choice == 2:
-            shared_params['CHANNEL_ID'] = int(input("Enter the channel ID: "))
-            shared_params['MESSAGE'] = input("Enter the message you want to spam: ")
-            shared_params['NUM_MESSAGES'] = int(input("Number of messages: "))
-            shared_params['PHOTO_URL'] = input("Enter an Image Address to send (leave empty if no photo): ").strip()
-            shared_params['DELAY_TIME'] = float(input("Enter the delay between messages in seconds: "))
-        elif choice == 3:
-            shared_params['CHANNEL_NAME'] = input("Enter the new channel name: ")
-            shared_params['CHANNEL_TYPE'] = input("Enter the type of the channel (text/voice): ").strip().lower()
-            shared_params['NUM_CHANNELS'] = int(input("Number of channels: "))
-        elif choice == 4:
-            pass
-        elif choice == 5:
-            shared_params['MESSAGE'] = input("Enter the message you want to send to all text channels: ")
-            shared_params['PHOTO_URL'] = input("Enter an Image Address to send (leave empty if no photo): ").strip()
-            shared_params['DELAY_TIME'] = float(input("Enter the delay between messages in seconds: "))
-            shared_params['NUM_MESSAGES_PER_CHANNEL'] = int(input("Enter number of messages per channel: "))
-        else:
-            print("Invalid choice.")
-            continue
+            if choice == 0:
+                break
+            if choice == 1:
+                shared_params['USER_ID'] = int(input("Enter the user ID to send messages to: "))
+                shared_params['MESSAGE'] = input("Enter the message you want to send: ")
+                shared_params['NUM_MESSAGES'] = int(input("Number of messages: "))
+                shared_params['PHOTO_URL'] = input("Enter an Image Address to send (leave empty if no photo): ")
+                shared_params['DELAY_TIME'] = float(input("Enter the delay between messages in seconds: "))
+            elif choice == 2:
+                shared_params['CHANNEL_ID'] = int(input("Enter the channel ID: "))
+                shared_params['MESSAGE'] = input("Enter the message you want to spam: ")
+                shared_params['NUM_MESSAGES'] = int(input("Number of messages: "))
+                shared_params['PHOTO_URL'] = input("Enter an Image Address to send (leave empty if no photo): ").strip()
+                shared_params['DELAY_TIME'] = float(input("Enter the delay between messages in seconds: "))
+            elif choice == 3:
+                shared_params['CHANNEL_NAME'] = input("Enter the new channel name: ")
+                shared_params['CHANNEL_TYPE'] = input("Enter the type of the channel (text/voice): ").strip().lower()
+                shared_params['NUM_CHANNELS'] = int(input("Number of channels: "))
+            elif choice == 4:
+                pass
+            elif choice == 5:
+                shared_params['MESSAGE'] = input("Enter the message you want to send to all text channels: ")
+                shared_params['PHOTO_URL'] = input("Enter an Image Address to send (leave empty if no photo): ").strip()
+                shared_params['DELAY_TIME'] = float(input("Enter the delay between messages in seconds: "))
+                shared_params['NUM_MESSAGES_PER_CHANNEL'] = int(input("Enter number of messages per channel: "))
+            else:
+                print("Invalid choice.")
+                continue
 
-        # Initialize and run all BOT clients
-        clients = [Client(intents=discord.Intents.default(), shared_params=shared_params, counters=counters) for _ in BOT_TOKENS]
-        await asyncio.gather(*(client.start(token) for client, token in zip(clients, BOT_TOKENS)))
+            # Initialize and run all BOT clients
+            clients = [Client(intents=discord.Intents.default(), shared_params=shared_params, counters=counters) for _ in BOT_TOKENS]
+            await asyncio.gather(*(client.start(token) for client, token in zip(clients, BOT_TOKENS)))
 
-        continue_choice = input("Continue? (Y/N): ").strip().lower()
-        if continue_choice != 'y':
-            print("Exiting the program. Goodbye! xD")
-            break
-        
+            counters = {"messages": 0, "channels": 0, "channels_deleted": 0}
+
+            continue_choice = input("Continue? (Y/N): ").strip().lower()
+            if continue_choice != 'y':
+                print("Exiting the program. Goodbye! xD")
+                break
+    
 asyncio.run(main())
